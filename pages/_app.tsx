b27277f403a33a65e187df2a242f3fbec2d15a13
Johnson Lin - IBM <Johnson.LIN@ibm.com>
2023-08-04 14:15:30 +0800

    Init Tailwind Project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import "../theme/styles.css";
-import theme from "../theme/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+import "../theme/global.css";
+
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
 
-export default MyApp;
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(<Component {...pageProps} />)
+}
