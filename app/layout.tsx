b95fee24988cd241f3b9139cebf9a672068599ff
Johnson Lin - IBM <Johnson.LIN@ibm.com>
2023-08-14 17:00:18 +0800

    Update Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import React from "react";
+import "../theme/global.css";
+import NavBar from "../components/nav/NavBar";
 
 type LayoutProps = {
     children: React.ReactNode,
 };
-
-import "../theme/global.css";
-import Link from "next/link";
 
 const Layout = ({
                     children
@@ -13,20 +12,16 @@ const Layout = ({
     return (
         <html lang="en">
         <body>
-            <div className="min-h-screen bg-gray-50">
-                {/* header */}
-                <div className="max-w-screen-lg mx-auto w-full flex py-4 justify-between">
-                    <Link href="/">Home</Link>
-                    <Link href="/about" prefetch={true}>About</Link>
-                </div>
-                {/* Body */}
-                <main
-                    className="">{children}</main>
-                {/* Footer */}
-                <div className="max-w-screen-lg w-full grid grid-cols-2 md:grid-cols-3 mx-auto">
-                    Footer
-                </div>
+        <div className="min-h-screen bg-gray-50">
+            {/* header */}
+            <NavBar/>
+            {/* Body */}
+            <main>{children}</main>
+            {/* Footer */}
+            <div className="max-w-screen-lg w-full grid grid-cols-2 md:grid-cols-3 mx-auto">
+                Footer
             </div>
+        </div>
         </body>
         </html>
     )
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 const NotFoundPage =()=>{
     return(
-        <div>
-            404 Page Not found
+        <div className="max-w-screen-lg mx-auto flex flex-col min-h-screen justify-center">
+            <div className="flex flex-col items-center gap-y-2">
+                <p className="font-bold text-4xl">404 Page not Found</p>
+                <Link href="/" prefetch={true} className="button">Go Home</Link>
+            </div>
         </div>
     )
 }
